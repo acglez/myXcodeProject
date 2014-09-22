437bb432a25a4b824bb84689542808ec8f75e1e1
Adrian <acglez10@gmail.com>
2014-09-22 17:06:09 +0100

    Initial Commit

---
+++ Roguelike_/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Roguelike_
+//
+//  Created by Adrián Calzadilla González on 22/09/14.
+//  Copyright (c) 2014 Adrián Calzadilla González. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
