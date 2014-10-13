7db1acf5d8c94cb116b2d3d119ae76d6e90a61cc
acalzadilla <acglez10@gmail.com>
2014-10-13 17:53:26 +0000

    Creada la clase Trol.

---
+++ Roguelike_/src/Trol.cpp
@@ -0,0 +1,10 @@
+#include "Trol.h"
+
+Trol::Trol():Actor(){
+    strong = 2;
+    life = 2;
+}
+
+Trol::~Trol(){
+    
+}
---
+++ Roguelike_/src/Trol.h
@@ -0,0 +1,7 @@
+#include "Actor.h"
+
+class Trol: public Actor {
+public:
+  Trol();
+  ~Trol();
+};
